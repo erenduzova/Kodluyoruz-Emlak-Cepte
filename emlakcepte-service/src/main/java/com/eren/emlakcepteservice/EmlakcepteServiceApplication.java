4c21a4a4539af0c35b08d1a64f7d818f03b2c703
Eren Düzova <erenduzova@gmail.com>
2023-01-03 23:11:06 +0300

    Initial Commit

---
+++ emlakcepte-service/src/main/java/com/eren/emlakcepteservice/EmlakcepteServiceApplication.java
@@ -0,0 +1,13 @@
+package com.eren.emlakcepteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmlakcepteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmlakcepteServiceApplication.class, args);
+	}
+
+}
---
+++ emlakcepte-service/src/test/java/com/eren/emlakcepteservice/EmlakcepteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eren.emlakcepteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmlakcepteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
