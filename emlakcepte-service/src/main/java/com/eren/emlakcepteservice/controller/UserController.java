e500e8d6ad29ef88d2ac4860aa4f05c589c0ce6f
Eren Düzova <erenduzova@gmail.com>
2023-01-06 20:02:36 +0300

    User Update Functionality Added

--- emlakcepte-service/src/main/java/com/eren/emlakcepteservice/controller/UserController.java
+++ emlakcepte-service/src/main/java/com/eren/emlakcepteservice/controller/UserController.java
@@ -2,10 +2,10 @@ package com.eren.emlakcepteservice.controller;
 
 import com.eren.emlakcepteservice.entity.User;
 import com.eren.emlakcepteservice.request.UserRequest;
+import com.eren.emlakcepteservice.request.UserUpdateRequest;
 import com.eren.emlakcepteservice.response.UserResponse;
 import com.eren.emlakcepteservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -40,14 +40,19 @@ public class UserController {
         return new ResponseEntity<>(userResponse, HttpStatus.FOUND);
     }
 
-    // Get User By Email ( if needed )
+    // Get User By Email
     @GetMapping(value = "/email/{email}")
     public ResponseEntity<UserResponse> getByEmail(@PathVariable String email) {
         UserResponse userResponse = userService.getUserResponseByEmail(email);
         return new ResponseEntity<>(userResponse, HttpStatus.FOUND);
     }
 
-    // Update Password
+    // Update User
+    @PutMapping(value = "/{userId}")
+    public ResponseEntity<UserResponse> update(@RequestBody UserUpdateRequest userUpdateRequest, @PathVariable Integer userId) {
+        UserResponse userResponse = userService.update(userUpdateRequest, userId);
+        return new ResponseEntity<>(userResponse, HttpStatus.OK);
+    }
 
 
 
--- emlakcepte-service/src/main/java/com/eren/emlakcepteservice/service/UserService.java
+++ emlakcepte-service/src/main/java/com/eren/emlakcepteservice/service/UserService.java
@@ -4,6 +4,7 @@ import com.eren.emlakcepteservice.converter.UserConverter;
 import com.eren.emlakcepteservice.entity.User;
 import com.eren.emlakcepteservice.repository.UserRepository;
 import com.eren.emlakcepteservice.request.UserRequest;
+import com.eren.emlakcepteservice.request.UserUpdateRequest;
 import com.eren.emlakcepteservice.response.UserResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -35,6 +36,7 @@ public class UserService {
         return userConverter.convert(userRepository.findAll());
     }
 
+    // Get UserResponse By Id
     public UserResponse getUserResponseById(Integer userId) {
         Optional<User> foundUser = getById(userId);
         return foundUser.map(user -> userConverter.convert(user))
@@ -44,14 +46,16 @@ public class UserService {
                 });
     }
 
+    // Get User By Id
     public Optional<User> getById(Integer userId) {
         return userRepository.findById(userId);
     }
+    // Get User By Email
     public Optional<User> getByEmail(String email) {
         return userRepository.findByEmail(email);
     }
 
-
+    // Get UserResponse By Email
     public UserResponse getUserResponseByEmail(String email) {
         Optional<User> foundUser = getByEmail(email);
         return foundUser.map(user -> userConverter.convert(user))
@@ -60,4 +64,28 @@ public class UserService {
                     return new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found with this email: " + email);
                 });
     }
+
+    // Update and return updated UserResponse
+    public UserResponse update(UserUpdateRequest userUpdateRequest, Integer userId) {
+        Optional<User> foundUser = getById(userId);
+        if (foundUser.isPresent()) {
+            User updatedUser = updateUser(foundUser.get(), userUpdateRequest);
+            return userConverter.convert(updatedUser);
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found with this id: " + userId);
+        }
+
+    }
+
+    // Update User
+    public User updateUser(User user, UserUpdateRequest userUpdateRequest) {
+        user.setName(userUpdateRequest.getName());
+        user.setEmail(userUpdateRequest.getEmail());
+        user.setPassword(userUpdateRequest.getPassword());
+        userRepository.save(user);
+        return user;
+    }
+
+
+
 }
