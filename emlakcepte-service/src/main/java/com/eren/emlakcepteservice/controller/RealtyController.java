4dc9875d89fc5ed504ae844697d5e4b00b17b465
Eren Düzova <erenduzova@gmail.com>
2023-01-07 00:01:22 +0300

    Realty Functionality
    Get All Realty endpoint and service method created.
    Create Realty endpoint and service method created.

--- emlakcepte-service/src/main/java/com/eren/emlakcepteservice/controller/RealtyController.java
+++ emlakcepte-service/src/main/java/com/eren/emlakcepteservice/controller/RealtyController.java
@@ -1,8 +1,14 @@
 package com.eren.emlakcepteservice.controller;
 
+import com.eren.emlakcepteservice.request.RealtyRequest;
+import com.eren.emlakcepteservice.response.RealtyResponse;
 import com.eren.emlakcepteservice.service.RealtyService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -12,6 +18,22 @@ public class RealtyController {
     @Autowired
     private RealtyService realtyService;
 
+    // Get All Realty
+    @GetMapping
+    public ResponseEntity<List<RealtyResponse>> getAllRealtyResponse() {
+        List<RealtyResponse> realtyResponses = realtyService.getAllRealtyResponse();;
+        return new ResponseEntity<>(realtyResponses, HttpStatus.OK);
+    }
 
+    // Create Realty
+    @PostMapping
+    public ResponseEntity<RealtyResponse> create(@RequestBody RealtyRequest realtyRequest) {
+        RealtyResponse savedRealty = realtyService.create(realtyRequest);
+        return new ResponseEntity<>(savedRealty, HttpStatus.CREATED);
+    }
+
+    // Get User's Active Realty
+
+    // Get User's All Realty
 
 }
--- emlakcepte-service/src/main/java/com/eren/emlakcepteservice/service/RealtyService.java
+++ emlakcepte-service/src/main/java/com/eren/emlakcepteservice/service/RealtyService.java
@@ -1,7 +1,39 @@
 package com.eren.emlakcepteservice.service;
 
+import com.eren.emlakcepteservice.converter.RealtyConverter;
+import com.eren.emlakcepteservice.entity.Realty;
+import com.eren.emlakcepteservice.entity.User;
+import com.eren.emlakcepteservice.repository.RealtyRepository;
+import com.eren.emlakcepteservice.request.RealtyRequest;
+import com.eren.emlakcepteservice.response.RealtyResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class RealtyService {
+
+    @Autowired
+    private RealtyRepository realtyRepository;
+
+    @Autowired
+    private RealtyConverter realtyConverter;
+
+    @Autowired
+    private UserService userService;
+
+    // Get All Realty
+    public List<RealtyResponse> getAllRealtyResponse() {
+        return realtyConverter.convert(realtyRepository.findAll());
+    }
+
+    // Create Realty
+    public RealtyResponse create(RealtyRequest realtyRequest) {
+        Realty newRealty = realtyConverter.convert(realtyRequest);
+        User user = userService.getById(realtyRequest.getUserId());
+        newRealty.setUser(user);
+        realtyRepository.save(newRealty);
+        return realtyConverter.convert(newRealty);
+    }
 }
