0cfd8e26575704fb17ac41d7473c644b70828a1a
Eren Düzova <erenduzova@gmail.com>
2023-01-06 23:59:50 +0300

    Realty Converter Created
    Converter for realty created.

--- emlakcepte-service/src/main/java/com/eren/emlakcepteservice/converter/RealtyConverter.java
+++ emlakcepte-service/src/main/java/com/eren/emlakcepteservice/converter/RealtyConverter.java
@@ -1,5 +1,45 @@
 package com.eren.emlakcepteservice.converter;
 
+import com.eren.emlakcepteservice.entity.Realty;
+import com.eren.emlakcepteservice.entity.enums.RealtyStatus;
+import com.eren.emlakcepteservice.request.RealtyRequest;
+import com.eren.emlakcepteservice.response.RealtyResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.List;
+@Component
 public class RealtyConverter {
+
+    @Autowired
+    private UserConverter userConverter;
+
+    public RealtyResponse convert(Realty realty) {
+        RealtyResponse response = new RealtyResponse();
+        response.setId(realty.getId());
+        response.setNo(realty.getNo());
+        response.setTitle(realty.getTitle());
+        response.setCreateDate(realty.getCreateDate());
+        response.setStatus(realty.getStatus());
+        response.setProvince(realty.getProvince());
+        response.setUserResponse(userConverter.convert(realty.getUser()));
+        return response;
+    }
+
+    public Realty convert(RealtyRequest realtyRequest){
+        Realty realty = new Realty();
+        realty.setNo(realtyRequest.getNo());
+        realty.setTitle(realtyRequest.getTitle());
+        realty.setProvince(realtyRequest.getProvince());
+        realty.setStatus(RealtyStatus.IN_REVIEW);
+        realty.setCreateDate(LocalDateTime.now());
+        // User ?
+        return realty;
+    }
+
+    public List<RealtyResponse> convert(List<Realty> allRealty) {
+        return allRealty.stream().map(this::convert).toList();
+    }
 
 }
