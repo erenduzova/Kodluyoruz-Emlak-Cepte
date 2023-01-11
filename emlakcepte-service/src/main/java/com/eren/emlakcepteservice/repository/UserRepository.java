93f3ecc74d47f2f5bcc7a6efbacbf939db035f7f
Eren Düzova <erenduzova@gmail.com>
2023-01-11 20:42:40 +0300

    Removed Unnecessary Imports From Repositories

--- emlakcepte-service/src/main/java/com/eren/emlakcepteservice/repository/UserRepository.java
+++ emlakcepte-service/src/main/java/com/eren/emlakcepteservice/repository/UserRepository.java
@@ -4,7 +4,6 @@ import com.eren.emlakcepteservice.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
